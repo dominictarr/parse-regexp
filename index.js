88b862e8b771b73171796a7d27f4dffa1fe15452
Dominic Tarr <dominic.tarr@gmail.com>
2012-10-24 17:17:56 +0200

    tidy

--- index.js
+++ index.js
@@ -1,11 +1,5 @@
-
-
-//var rx = /^/\([\/^[\\\/]]+)\/(.*)$/
 var rx = /^\/((?:\\\/|[^\/])+)\/(.*)$/
-
 module.exports = function (string) {
-
   var m = rx.exec(string)
   return m ? new RegExp(m[1], m[2]) : null
-
 }
